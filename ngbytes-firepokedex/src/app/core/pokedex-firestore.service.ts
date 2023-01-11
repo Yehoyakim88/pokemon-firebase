1c900bb66e9a5d69b147519c59bd30d945539add
Yehoyakim88 <yehoyakim@mail.de>
2023-01-11 08:31:20 +0100

    implementation of interface functions

--- ngbytes-firepokedex/src/app/core/pokedex-firestore.service.ts
+++ ngbytes-firepokedex/src/app/core/pokedex-firestore.service.ts
@@ -1,12 +1,57 @@
 import { Injectable } from '@angular/core';
-import { collection, CollectionReference, DocumentData, Firestore } from '@angular/fire/firestore';
+import { addDoc, collection, collectionData, CollectionReference, deleteDoc, doc, docData, DocumentData, Firestore, updateDoc } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+
+interface Pokemon {
+  height: number;
+  id: string;
+  description: string;
+  imgUrl: string;
+  name: string;
+  type: string;
+  weight: number;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class PokedexFirestoreService {
   private pokemonCollection: CollectionReference<DocumentData>;
+
   constructor(private readonly firestore: Firestore) {
     this.pokemonCollection = collection(this.firestore, 'pokemon');
    }
+
+   
+   getAll() {
+    return collectionData(this.pokemonCollection, {
+      idField: 'id',
+    }) as Observable<Pokemon[]>;
+  }
+
+
+  get(id: string) {
+    const pokemonDocumentReference = doc(this.firestore, `pokemon/${id}`);
+    return docData(pokemonDocumentReference, { idField: 'id' });
+  }
+
+
+  create(pokemon: Pokemon) {
+    return addDoc(this.pokemonCollection, pokemon);
+  }
+
+
+  update(pokemon: Pokemon) {
+    const pokemonDocumentReference = doc(
+      this.firestore,
+      `pokemon/${pokemon.id}`
+    );
+    return updateDoc(pokemonDocumentReference, { ...pokemon });
+  }
+
+
+  delete(id: string) {
+    const pokemonDocumentReference = doc(this.firestore, `pokemon/${id}`);
+    return deleteDoc(pokemonDocumentReference);
+  }
 }
